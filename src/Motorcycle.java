80ca8351a404ee34772d96633ff752ffc716bb10
Адлет <adlet2007094@gmail.com>
2025-12-29 00:56:18 +0500

    feat: create abstract Motorcycle subclass of Vehicle and display info of Motorcycle and a side car

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,2 +1,22 @@
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+    boolean hasSidecar;
+    public Motorcycle(String brand,int year,boolean hasSidecar) {
+        super(brand,year);
+        this.hasSidecar=hasSidecar;
+    }
+
+    @Override
+    void startEngine(){
+        System.out.println(brand + " engine started");
+    }
+
+    @Override
+    void stopEngine(){
+        System.out.println(brand + " engine stopped");
+    }
+
+    void displayMotorcycleInfo() {
+        displayInfo();
+        System.out.println("Has a side car? :"+hasSidecar);
+    }
 }

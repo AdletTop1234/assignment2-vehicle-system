c91944dc9c6138d578231e25bcb87a3b5b8c52b2
Адлет <adlet2007094@gmail.com>
2025-12-28 21:00:36 +0500

    feat: create abstract Vehicle class with displayInfo

---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+abstract class Vehicle    {
+    protected String brand;
+    protected int year;
+
+    Vehicle(String brand,int year){
+        this.brand=brand;
+        this.year=year;
+    }
+
+    abstract void startEngine();
+    abstract void stopEngine();
+
+    void displayInfo(){
+        System.out.println("Brand :"+brand+", Year:"+year);
+    }
+}

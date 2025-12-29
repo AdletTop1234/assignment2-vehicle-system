2d09e5f535107f328c1378fa414c16872976a1f9
Адлет <adlet2007094@gmail.com>
2025-12-29 12:43:01 +0500

    feat: create abstract Truck subclass with info about capacity and number of Axles

--- src/Truck.java
+++ src/Truck.java
@@ -1,2 +1,25 @@
-public class Truck {
+public class Truck extends Vehicle{
+    double capacity;
+    int numAxles;
+
+    public Truck(String brand,int year,double capacity,int numAxles){
+        super(brand,year);
+        this.capacity=capacity;
+        this.numAxles=numAxles;
+    }
+
+    @Override
+    void startEngine(){
+        System.out.println(brand+" Engine started");
+    }
+
+    @Override
+    void stopEngine(){
+        System.out.println(brand+" Engine stopped");
+    }
+
+    void displayTruckInfo() {
+        displayInfo();
+        System.out.println("Capacity :"+capacity+"Number of Axles :"+numAxles);
+    }
 }
